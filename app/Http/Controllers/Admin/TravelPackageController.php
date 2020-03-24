bc4bf4a2df55faa6eccfa9a8629d86598e817401
Iqbalaulia <16410100012@stikom.edu>
2020-03-24 22:28:21 +0700

    CRUD Edit Travel Package

--- app/Http/Controllers/Admin/TravelPackageController.php
+++ app/Http/Controllers/Admin/TravelPackageController.php
@@ -59,7 +59,7 @@ class TravelPackageController extends Controller
      */
     public function show($id)
     {
-        //
+        
     }
 
     /**
@@ -70,7 +70,11 @@ class TravelPackageController extends Controller
      */
     public function edit($id)
     {
-        //
+        $item = TravelPackage::findOrFail($id);
+
+        return view('pages.admin.travel-package.edit',[
+            'item' => $item
+        ]);
     }
 
     /**
@@ -80,9 +84,17 @@ class TravelPackageController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(TravelPackageRequest $request, $id)
     {
-        //
+        $data = $request->all();
+
+        $data['slug']= Str::slug($request->title);
+
+        $item = TravelPackage::findOrFail($id);
+
+        $item->update($data);
+        
+        return redirect()->route('travel-package.index');
     }
 
     /**
