319b0f65d93d6a9d677d657cace767798e8b7364
Iqbalaulia <16410100012@stikom.edu>
2020-03-28 16:40:58 +0700

    New page forgot password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,75 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+    <title>Document</title>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <link rel="stylesheet" href="{{ asset('../frontend/libraries/bootstrap/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('../frontend/libraries/xzoom/xzoom.css') }}">
+    <link rel="stylesheet" href="{{ asset('../frontend/libraries/gijgo/css/gijgo.min.css') }}">
+    <link
+        href="https://fonts.googleapis.com/css?family=Assistant:200,300,400,600,700,800|Playfair+Display:400,400i,700,700i,900,900i&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('../frontend/styles/main.css') }}">
+</head>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+<body>
+    <main class="forgot-password-container">
+        <div class="container">
+            <div class="row page-forgot-password d-flex align-items-center">
+                <div class="section-left col-12 col-md-6">
+                    <h1 class="mb-4">
+                        Wow,
+                        <br>
+                        I forgot my password !
+                    </h1>
 
-                                @error('email')
+                    <img src="{{ asset('../frontend/images/forgot-password.png') }}" class="w-75 d-none d-sm-flex"
+                        alt="">
+                </div>
+                <div class="section-right col-12 col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="text-center">
+                                <img src="{{ asset('../frontend/images/logo_nomads.png') }}" class="w-50 mb-4" alt="">
+                            </div>
+
+                            <form method="POST" action="{{ route('password.email') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Email address</label>
+                                    <input id="email" type="email"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                    @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-                        </div>
+                                    @enderror
+                                </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
+                                <button type="submit" class="btn btn-login btn-block">Send Password Reset Link</button>
+
+                            </form>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+    </main>
+
+
+    <script src="{{ asset('../frontend/libraries/jquery/jquery-3.4.1.min.js') }}"></script>
+    <script src="{{ asset('../frontend/libraries/bootstrap/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('../frontend/libraries/retina/retina.min.js') }}"></script>
+
+
+</body>
+
+</html>
